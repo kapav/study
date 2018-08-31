884d51102d161331756c2186db7ea4828334b320
ARTGROUP\akochkin <Alexander.Kochkin@artezio.com>
2018-08-31 09:36:57 +0400

    Added several variables to access the form.

--- webvideo/js/main.js
+++ webvideo/js/main.js
@@ -1,18 +1,16 @@
 (function() {
 	'use strict';
-	var folder = "video/",
+	var vidForm = document.forms.vidForm,
+		vidInput = vidForm[0],
+		vidSubmit = vidForm[1],
+		folder = "video/",
 		tableLayout = $("#tableLayout"),
 		rowTemplateFunc = _.template($('#rowTemplate').html());
-		
-	function renderRows() {
-		var html = rowTemplateFunc({  });
-		tableLayout.append(html);
-	}
 		
 	$.ajax({
 		url: folder,
 		success: function (data) {
-			$(data).find("a").attr("href", function(indexOfRow, filename) {
+			$(data).find('a').attr('href', function(indexOfRow, filename) {
 				var html = rowTemplateFunc({
 					indexOfRow: indexOfRow,
 					folder: folder,
