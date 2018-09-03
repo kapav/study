b303f13283288fdad301a3750f415f46c9a3eb5a
ARTGROUP\akochkin <Alexander.Kochkin@artezio.com>
2018-09-03 10:26:44 +0400

    Added Current time value in the Progress bar.

--- webvideo/js/main.js
+++ webvideo/js/main.js
@@ -7,14 +7,45 @@ window.onload = (function() {
 		$tableLayout = $("#tableLayout"),
 		rowTemplateFunc = _.template($('#rowTemplate').html());
 		
+	function timeFormat(timeInSeconds) {
+		var reminderAfterHours = timeInSeconds % 3600,
+			hours = Math.round((timeInSeconds - reminderAfterHours) / 3600),
+			reminderAfterMinutes = reminderAfterHours % 60,
+			minutes = Math.round((reminderAfterHours - reminderAfterMinutes) / 60),
+			seconds = Math.round(reminderAfterMinutes);
+			
+		minutes = ('00' + minutes).slice(-2);
+		seconds = ('00' + seconds).slice(-2);
+		return `${hours}:${minutes}:${seconds}`;
+	}
+		
+	function timeupdateInialize($elem) {
+		$elem.attr('style', 'width: 0%; color: #f00;');
+	}
+	
+	function timeupdateHandler($video, $progressBar) {
+		var currentTimeOfVideo = $video[0].currentTime;
+		
+		$progressBar.attr('style', `width: ${Math.round(100 * currentTimeOfVideo / $video[0].duration)}%; color: #f00;`);
+		$progressBar.html(timeFormat(currentTimeOfVideo));
+	}
+		
 	function tableHandler(event) {
 		var $target = $(event.target),
-			html = $target.html(),
-			$video = $target.closest('tr').find('video');
+			htmlOfButton = null,
+			$video = null,
+			durationOfVideo = null,
+			$progressBar = null;
 		
 		if ($target.hasClass('play-button')) {
+			htmlOfButton = $target.html();
+			$video = $target.closest('tr').find('video');
+			durationOfVideo = $video.duration;
+			$progressBar = $target.closest('tr').find('.play-progress-bar');
+			
+			timeupdateInialize($progressBar);
 			$target.toggleClass('btn-success btn-warning');
-			$target.html(html === 'Start' ? 'Pause' : 'Start');
+			$target.html(htmlOfButton === 'Start' ? 'Pause' : 'Start');
 			
 			if (!$video[0].error) {
 				if ($video[0].paused) {
@@ -23,10 +54,12 @@ window.onload = (function() {
 					$video[0].pause();
 				}
 			}
+			
+			$video.on('timeupdate', timeupdateHandler.bind(null, $video, $progressBar));
 		}
 	}
 
-		$tableLayout.append(
+	$tableLayout.append(
 		rowTemplateFunc({
 			indexOfRow: 100,
 			folder: folder,
