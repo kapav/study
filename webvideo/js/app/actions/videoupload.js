74a9e4714b31385a2311496fee71383bd0d1a794
ARTGROUP\akochkin <Alexander.Kochkin@artezio.com>
2018-09-06 12:50:05 +0400

    1

---
+++ webvideo/js/app/actions/videoupload.js
@@ -0,0 +1,32 @@
+define(['lodash'], (_) => {
+	'use strict';
+	var	rowTemplateFunc = _.template($('#rowTemplate').html()),
+		currentCount = 1;
+		
+	function getCurrentCount() {
+		return currentCount++;
+	}
+		
+	function videoUpload($vidForm, $tableBody, event) {
+		var files = event.target[0].files,
+			wrapper = document.getElementById('wrapper'),
+			video = null,
+			source = null,
+			i;
+		
+		for (i = 0; i < files.length; i++) {
+			$tableBody.append(
+				rowTemplateFunc({
+					indexOfRow: getCurrentCount(),
+					blob: window.URL.createObjectURL(files[i]),
+					filename: files[i].name
+				})
+			);
+		}
+		
+		event.preventDefault();
+		$vidForm.trigger('reset');
+	}
+		
+	return videoUpload;
+});
