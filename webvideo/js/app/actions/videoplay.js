440d7a8ba3fbec780ffaf8d164a16c64b9353000
ARTGROUP\akochkin <Alexander.Kochkin@artezio.com>
2018-09-06 14:16:15 +0400

    Added ES6 module.

--- webvideo/js/app/actions/videoplay.js
+++ webvideo/js/app/actions/videoplay.js
@@ -1,83 +1,77 @@
-define(() => {
-	'use strict';
+function timeFormat(timeInSeconds) {
+	var reminderAfterHours = timeInSeconds % 3600,
+		hours = Math.round((timeInSeconds - reminderAfterHours) / 3600),
+		reminderAfterMinutes = reminderAfterHours % 60,
+		minutes = Math.round((reminderAfterHours - reminderAfterMinutes) / 60),
+		seconds = Math.round(reminderAfterMinutes);
+		
+	minutes = ('00' + minutes).slice(-2);
+	seconds = ('00' + seconds).slice(-2);
+	return `${hours}:${minutes}:${seconds}`;
+}
 
-	function timeFormat(timeInSeconds) {
-		var reminderAfterHours = timeInSeconds % 3600,
-			hours = Math.round((timeInSeconds - reminderAfterHours) / 3600),
-			reminderAfterMinutes = reminderAfterHours % 60,
-			minutes = Math.round((reminderAfterHours - reminderAfterMinutes) / 60),
-			seconds = Math.round(reminderAfterMinutes);
-			
-		minutes = ('00' + minutes).slice(-2);
-		seconds = ('00' + seconds).slice(-2);
-		return `${hours}:${minutes}:${seconds}`;
-	}
+function playerInitialize($startButton, $progressBar) {
+	$startButton.html('Start');
+	$startButton
+		.removeClass('btn-success btn-warning btn-danger')
+		.addClass('btn-success');
+	$progressBar.attr('style', 'width: 0%; color: #f00;');
+	$progressBar.html('0:00:00');
+}
 	
-	function playerInitialize($startButton, $progressBar) {
-		$startButton.html('Start');
-		$startButton
-			.removeClass('btn-success btn-warning btn-danger')
-			.addClass('btn-success');
-		$progressBar.attr('style', 'width: 0%; color: #f00;');
-		$progressBar.html('0:00:00');
-	}
+function timeupdateHandler($video, $progressBar) {
+	var currentTimeOfVideo = $video[0].currentTime;
+	
+	$progressBar.attr('style', `width: ${Math.round(100 * currentTimeOfVideo / $video[0].duration)}%; color: #f00;`);
+	$progressBar.html(timeFormat(currentTimeOfVideo));
+}
+
+function endedHandler($startButton, $progressBar, $video) {
+	playerInitialize($startButton, $progressBar);
+	$video[0].fastSeek(0);
+}
+
+function videoInialize($startButton) {
+	var $progressBar = $startButton.closest('tr').find('.play-progress-bar'),
+		$video = $startButton.closest('tr').find('video');
+	
+	playerInitialize($startButton, $progressBar);
+	$video
+		.off('timeupdate')
+		.on('timeupdate', timeupdateHandler.bind(null, $video, $progressBar))
+		.off('ended')
+		.on('ended', endedHandler.bind(null, $startButton, $progressBar, $video));
+}
+
+function startHandler($startButton) {
+	var	htmlOfButton = $startButton.html(),
+		$video = $startButton.closest('tr').find('video'),
+		durationOfVideo = $video.duration,
+		startPositionFlag = true;
 		
-	function timeupdateHandler($video, $progressBar) {
-		var currentTimeOfVideo = $video[0].currentTime;
-		
-		$progressBar.attr('style', `width: ${Math.round(100 * currentTimeOfVideo / $video[0].duration)}%; color: #f00;`);
-		$progressBar.html(timeFormat(currentTimeOfVideo));
-	}
-	
-	function endedHandler($startButton, $progressBar, $video) {
-		playerInitialize($startButton, $progressBar);
-		$video[0].fastSeek(0);
-	}
-	
-	function videoInialize($startButton) {
-		var $progressBar = $startButton.closest('tr').find('.play-progress-bar'),
-			$video = $startButton.closest('tr').find('video');
-		
-		playerInitialize($startButton, $progressBar);
-		$video
-			.off('timeupdate')
-			.on('timeupdate', timeupdateHandler.bind(null, $video, $progressBar))
-			.off('ended')
-			.on('ended', endedHandler.bind(null, $startButton, $progressBar, $video));
-	}
-	
-	function startHandler($startButton) {
-		var	htmlOfButton = $startButton.html(),
-			$video = $startButton.closest('tr').find('video'),
-			durationOfVideo = $video.duration,
-			startPositionFlag = true;
-			
-		if (!$video[0].error) {
-			if ($video[0].paused) {
-				if (startPositionFlag)
-				$startButton
-					.removeClass('btn-success btn-warning btn-danger')
-					.addClass('btn-danger');
-				$startButton.html('Pause');
-				$video[0].play();
-			} else {
-				$startButton
-					.removeClass('btn-success btn-warning btn-danger')
-					.addClass('btn-warning');
-				$startButton.html('Resume');
-				$video[0].pause();
-			}
+	if (!$video[0].error) {
+		if ($video[0].paused) {
+			if (startPositionFlag)
+			$startButton
+				.removeClass('btn-success btn-warning btn-danger')
+				.addClass('btn-danger');
+			$startButton.html('Pause');
+			$video[0].play();
+		} else {
+			$startButton
+				.removeClass('btn-success btn-warning btn-danger')
+				.addClass('btn-warning');
+			$startButton.html('Resume');
+			$video[0].pause();
 		}
 	}
-		
-	function videoPlay(event) {
-		var $target = $(event.target);
-		
-		if ($target.hasClass('play-button')) {
-			videoInialize($target);
-			startHandler($target);
-		}
-	}
+}
 	
-	return videoPlay;
-});
+export default function videoPlay(event) {
+	var $target = $(event.target);
+	
+	if ($target.hasClass('play-button')) {
+		videoInialize($target);
+		startHandler($target);
+	}
+}
--- webvideo/js/app/main.js
+++ webvideo/js/app/main.js
@@ -1,13 +1,12 @@
-require(['jquery', 'actions/videoplay', 'actions/videoupload'], ($, videoPlay, videoUpload) => {
-    'use strict';
-	var $vidForm = $('#vidForm'),
-		$tableBody = $("#tableBody");
-		
-	window.onload = (function() {
-		$vidForm.on(
-			'submit',
-			videoUpload.bind(null, $vidForm, $tableBody)
-		);
-		$tableBody.on('click', videoPlay);
-	})();
-});
+import videoUpload from './actions/videoupload.js';
+import videoPlay from './actions/videoplay.js';
+const $vidForm = $('#vidForm'),
+	$tableBody = $('#tableBody');
+	
+window.onload = (function() {
+	$vidForm.on(
+		'submit',
+		videoUpload.bind(null, $vidForm, $tableBody)
+	);
+	$tableBody.on('click', videoPlay);
+})();
