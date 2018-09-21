8988af93583f628443231c614b88c3c770c21af2
kapav <apam@ro.ru>
2018-09-21 11:54:34 +0400

    The 'bundle.js' file created, but 'requirejs is not defined'.

--- webvideo/webpack.config.js
+++ webvideo/webpack.config.js
@@ -1,5 +1,11 @@
 'use strict';
+const path = require('path');
 
 module.exports = {
-	
+  mode: 'development',
+  entry: './js/config.js',
+  output: {
+    path: path.resolve(__dirname, 'dist', 'js'),
+    filename: 'bundle.js'
+  }	
 }
