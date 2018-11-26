7c8c567ef3cc8381a64b72cabc184971558eb771
kapav <apam@ro.ru>
2018-11-26 16:49:20 +0400

    'webpack.DefinePlugin' added.

--- webvideo/src/js/app/main.js
+++ webvideo/src/js/app/main.js
@@ -3,10 +3,14 @@
 
 import videoUpload from './actions/videoupload';
 import videoPlay from './actions/videoplay';
+
+if (NODE_ENV == 'development') {
+	console.log('NODE_ENV from main.js', NODE_ENV);
+}
 	
 /**
 * Performs the initial loading of the page
-**/	
+**/
 function bootstrap() {
 	var vidForm = document.getElementById('vidForm');
 	var tableBody = document.getElementById('tableBodyMovie');
--- webvideo/webpack.config.js
+++ webvideo/webpack.config.js
@@ -1,8 +1,11 @@
 'use strict';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
 const path = require('path');
 
+console.log('NODE_ENV', NODE_ENV);
 module.exports = {
-  mode: 'development',
+  mode: NODE_ENV,
   entry: {
     main: [
       './src/js/app/main.js',
@@ -15,5 +18,11 @@ module.exports = {
   },
   watchOptions: {
     ignored: ['node_modules', 'bower_components', 'dist', 'src/*.html']
-  }
+  },
+  devtool: NODE_ENV == 'development' ? 'inline-source-map' : false,
+  plugins: [
+	new webpack.DefinePlugin({
+		'NODE_ENV': JSON.stringify(NODE_ENV)
+	})
+  ]
 }
